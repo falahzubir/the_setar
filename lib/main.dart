096c9a74b593d13d811f928ed8b957462a52b1c9
Falah Ahmad Zubir <falah.ahmadzubir98@gmail.com>
2023-07-16 00:33:06 +0800

    Add splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 // import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:the_setar/auth/login_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   // await Firebase.initializeApp();
+
+  await Future.delayed(const Duration(seconds: 3));
+  FlutterNativeSplash.remove;
+
   runApp(const MyApp());
 
   SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
