be6c2fa292e968ac83513b8e328972e7be2a178f
Falah Ahmad Zubir <falah.ahmadzubir98@gmail.com>
2023-07-22 16:41:57 +0800

    Update image

--- lib/model/data_model.dart
+++ lib/model/data_model.dart
@@ -19,17 +19,17 @@ class UserData {
 // Data untuk food page
 UserData laksaData = UserData(
   'assets/laksa.png',
-  'assets/laksa.png',
-  'assets/laksa.png',
-  'assets/laksa.png',
+  'assets/laksa1.png',
+  'assets/laksa2.png',
+  'assets/laksa3.png',
   'Laksa Tepi Sungai',
   'Laksa Tepi Sungai Kedah is a popular local dish in the Malaysian state of Kedah. It is a variant of laksa, which is a traditional spicy noodle soup commonly found in Southeast Asia. The name "Laksa Tepi Sungai" translates to "Laksa by the riverside" in English, indicating that it is a dish commonly enjoyed near the river in Kedah. The dish typically consists of rice noodles served in a flavorful broth made from a combination of spices, herbs, and various ingredients.',
 );
 
 UserData meeAbu = UserData(
   'assets/mee_abu.jpg',
-  'assets/mee_abu.jpg',
-  'assets/mee_abu.jpg',
+  'assets/mee_abu1.png',
+  'assets/mee_abu2.png',
   'assets/mee_abu.jpg',
   'Mee Abu',
   'Mee Abu Alor Star features yellow egg noodles, commonly known as "mee kuning" or "mee jawa," which are cooked to perfection. The noodles are typically stir-fried with a combination of ingredients, resulting in a flavorful and aromatic dish.',
@@ -37,18 +37,18 @@ UserData meeAbu = UserData(
 
 UserData nasiLemak = UserData(
   'assets/nasik_lemak_hj_ali.png',
-  'assets/nasik_lemak_hj_ali.png',
-  'assets/nasik_lemak_hj_ali.png',
-  'assets/nasik_lemak_hj_ali.png',
+  'assets/nasik_lemak_hj_ali1.png',
+  'assets/nasik_lemak_hj_ali2.png',
+  'assets/nasik_lemak_hj_ali3.png',
   'Nasi Lemak Haji Ali',
   'Nasi Lemak Haji Ali is a well-known restaurant in Alor Setar, Malaysia that specializes in serving Nasi Lemak, a popular Malaysian dish. It is a beloved spot among locals and visitors alike who seek authentic and flavorful Malaysian cuisine.',
 );
 
 UserData rumahMengambing = UserData(
   'assets/rumah_mengambing.png',
-  'assets/rumah_mengambing.png',
-  'assets/rumah_mengambing.png',
-  'assets/rumah_mengambing.png',
+  'assets/rumah_mengambing1.png',
+  'assets/rumah_mengambing2.png',
+  'assets/rumah_mengambing3.png',
   'Rumah Mengambing',
   'Warung Mengambing is known for its tender and flavorful goat dishes, and they aim to provide an enjoyable dining experience for their customers. The restaurant\'s ambiance is typically casual and welcoming, with a focus on showcasing the flavors of Malay cuisine.',
 );
@@ -83,9 +83,9 @@ UserData rumahMerdeka = UserData(
 
 UserData kedahRoyalMuseum = UserData(
   'assets/historical/royal_museum.png',
-  'assets/historical/royal_museum.png',
-  'assets/historical/royal_museum.png',
-  'assets/historical/royal_museum.png',
+  'assets/royal_museum1.png',
+  'assets/royal_museum2.png',
+  'assets/royal_museum3.png',
   'Kedah Royal Museum',
   'The Kedah Royal Museum is housed in the former royal palace, which was the official residence of the Kedah Sultanate until 1922. The museum offers visitors a unique opportunity to explore the royal artifacts, regalia, historical documents, photographs, and personal belongings of the Kedah royal family. Inside the museum, you can explore various sections that provide insights into the history, culture, and traditions of the Kedah Sultanate. The exhibits include royal costumes, jewelry, weaponry, ceremonial items, and other artifacts that showcase the grandeur and splendor of the Kedah royal court. The museum also features reconstructions of royal chambers, including the bedrooms, reception halls, and ceremonial spaces, offering visitors a glimpse into the royal lifestyle.',
 );
@@ -93,18 +93,18 @@ UserData kedahRoyalMuseum = UserData(
 // Data untuk commercial page
 UserData amanCentral = UserData(
   'assets/aman.jpg',
-  'assets/aman.jpg',
-  'assets/aman.jpg',
-  'assets/aman.jpg',
+  'assets/aman1.png',
+  'assets/aman2.png',
+  'assets/aman3.png',
   'Aman Central',
   'Aman Central is known for its diverse selection of stores, with both local and international brands represented. The mall features various fashion retailers, including clothing, footwear, accessories, and beauty products. Additionally, you can find electronics, home decor, sports equipment, books, and more. The mall also boasts a wide range of dining options, including local Malaysian cuisine, international food chains, cafes, and fast-food outlets. Whether you\'re looking for traditional Malaysian dishes, Western cuisine, or Asian fusion, Aman Central offers a variety of dining choices to suit different tastes.',
 );
 
 UserData pekanRabu = UserData(
   'assets/pekan_rabu.jpg',
-  'assets/pekan_rabu.jpg',
-  'assets/pekan_rabu.jpg',
-  'assets/pekan_rabu.jpg',
+  'assets/pekan_rabu1.png',
+  'assets/pekan_rabu2.png',
+  'assets/pekan_rabu3.png',
   'Pekan Rabu',
   'Pekan Rabu, also known as "Wednesday Market," is a famous landmark and traditional market in Alor Setar, the capital city of the Malaysian state of Kedah. It is a popular destination among locals and tourists alike, known for its diverse range of products and vibrant atmosphere. The market is particularly renowned for its variety of traditional Malay snacks and delicacies. You can find items such as kuih (local pastries), dodol (a sticky sweet treat), keropok (crackers), and various local snacks that represent the rich culinary heritage of the region. Aside from food, Pekan Rabu also offers traditional arts and crafts, including batik fabric, woven baskets, pottery, traditional clothing, and other unique items that reflect the local craftsmanship and cultural traditions.',
 );
@@ -112,17 +112,17 @@ UserData pekanRabu = UserData(
 // Data untuk recreation page
 UserData dataranJubliMas = UserData(
   'assets/dataran_jubli_mas.png',
-  'assets/dataran_jubli_mas.png',
-  'assets/dataran_jubli_mas.png',
-  'assets/dataran_jubli_mas.png',
+  'assets/dataran_jubli_mas1.png',
+  'assets/dataran_jubli_mas2.png',
+  'assets/dataran_jubli_mas3.png',
   'Dataran Jubli Emas',
   'Dataran Jubli Emas, also known as the Golden Jubilee Square, is a prominent public square located in Alor Setar, the capital city of Kedah, Malaysia. The square was built to commemorate the 50th anniversary of the reign of Sultan Abdul Halim Mu\'adzam Shah, the 14th Sultan of Kedah. Dataran Jubli Emas is a popular gathering place and a landmark in Alor Setar. The square is characterized by its iconic and distinctive golden dome structure, which represents the Jubilee celebrations.',
 );
 
 UserData dataranPahlawan = UserData(
   'assets/dataran_pahlawan.png',
-  'assets/dataran_pahlawan.png',
-  'assets/dataran_pahlawan.png',
+  'assets/dataran_pahlawan1.png',
+  'assets/dataran_pahlawan2.png',
   'assets/dataran_pahlawan.png',
   'Dataran Pahlawan',
   'Dataran Pahlawan is a square in Kedah. Dataran Pahlawan is situated nearby to Kampung Warisan and the golf course Royal Kedah Club. Dataran Pahlawan, also known as Warrior Square, is a significant landmark in Alor Setar. It is situated in the heart of the city and holds historical and cultural significance. The square serves as a gathering place and a venue for various events, festivals, and celebrations. Dataran Pahlawan features a central fountain surrounded by beautifully landscaped gardens and walkways. The square\'s design incorporates elements that reflect the rich cultural heritage of Kedah, and it provides a serene environment for residents and visitors to enjoy.',
@@ -130,9 +130,9 @@ UserData dataranPahlawan = UserData(
 
 UserData dataranMasjidZahir = UserData(
   'assets/dataran_masjid_zahir.png',
-  'assets/dataran_masjid_zahir.png',
-  'assets/dataran_masjid_zahir.png',
-  'assets/dataran_masjid_zahir.png',
+  'assets/dataran_masjid_zahir1.png',
+  'assets/dataran_masjid_zahir2.png',
+  'assets/dataran_masjid_zahir3.png',
   'Dataran Masjid Zahir',
   'Dataran Masjid Zahir, also known as Zahir Mosque Square, is a prominent public square located in Alor Setar, the capital city of Kedah, Malaysia. It is situated in front of the iconic Zahir Mosque, one of the oldest and most significant mosques in Malaysia. Dataran Masjid Zahir serves as a focal point for both locals and tourists visiting the Zahir Mosque. The square offers a spacious and well-maintained area with paved walkways, green spaces, and seating areas. It provides a place for people to relax, take in the view of the mosque, and appreciate the architectural beauty of the surrounding area.',
 );
